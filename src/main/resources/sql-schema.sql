5e541224bdf06ee4dd12cc51c5a51675ef2dbae2
Charles Cairney <107991366+CSCairney@users.noreply.github.com>
2022-07-19 14:53:32 +0100

    Updated Schema for base Dev Branch

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,32 +1,33 @@
-drop schema ims;
-
 CREATE SCHEMA IF NOT EXISTS `ims`;
+USE `ims`;
 
-USE `ims` ;
+DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `items`;
+DROP TABLE IF EXISTS `order_items`;
 
-CREATE TABLE IF NOT EXISTS `ims`.`customers`(
-`customer_id` INT PRIMARY KEY AUTO_INCREMENT,
-`customer_firstname` VARCHAR(40) NOT NULL,
-`customer_surname` VARCHAR(40) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `ims`.`customer` (
+	`customer_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `customer_firstname` VARCHAR (20) NOT NULL,
+    `customer_surname` VARCHAR (20) NOT NULL
+    );
 
-CREATE TABLE IF NOT EXISTS `ims`.`items`(
-`item_id` INT PRIMARY KEY AUTO_INCREMENT,
-`item_name` VARCHAR(20) NOT NULL,
-`item_price` DOUBLE NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+	`item_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `item_name` VARCHAR (100) NOT NULL,
+    `item_price` DOUBLE NOT NULL
+    );
 
-CREATE TABLE IF NOT EXISTS `ims`.`orders`(
-`order_id` INT PRIMARY KEY AUTO_INCREMENT,
-`fk_customer_id` INT NOT NULL,
-FOREIGN KEY (`fk_customer_id`) REFERENCES `customers`(`customer_id`)
-);
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+	`order_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `fk_customer_id` INT NOT NULL,
+    FOREIGN KEY (`fk_customer_id`) REFERENCES `customer`(`customer_id`) ON DELETE CASCADE
+    );
 
-CREATE TABLE IF NOT EXISTS `ims`.`order_items`(
-`orderitem_id` INT PRIMARY KEY AUTO_INCREMENT,
-`fk_order_id` INT NOT NULL,
-`fk_item_id` INT NOT NULL,
-`order_cost` INT NOT NULL,
-FOREIGN KEY (`fk_order_id`) REFERENCES `orders`(`order_id`),
-FOREIGN KEY (`fk_item_id`) REFERENCES `items`(`item_id`)
-);
+CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
+	`orderitem_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `fk_order_id` INT NOT NULL,
+    `fk_item_id` INT NOT NULL,
+    FOREIGN KEY (`fk_order_id`) REFERENCES `orders`(`order_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`fk_item_id`) REFERENCES `items`(`item_id`) ON DELETE CASCADE
+    );

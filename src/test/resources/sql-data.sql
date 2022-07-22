057253a506cab7a8f8567cd41d04378b0073f591
Charles Cairney <107991366+CSCairney@users.noreply.github.com>
2022-07-22 01:24:19 +0100

    Update sql-data.sql

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,3 +1,3 @@
 INSERT INTO `customers` (`customer_firstname`, `customer_surname`) VALUES ('jordan', 'harrison');
 INSERT INTO `items` (`item_name`, `item_price`) VALUES ('dragon', 200.50);
-INSERT INTO `orders` (`customer_id`) VALUES (1);
+INSERT INTO `orders` (`fk_customer_id`) VALUES (1);
